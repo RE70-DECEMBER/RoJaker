23b4e0020d71a39456fd6ba02c23cbc9dd10ca4c
RE70-DECEMBER <158739296+RE70-DECEMBER@users.noreply.github.com>
2024-02-03 05:00:51 -0600

    Add files via upload
    Python Roku

---
+++ rojaker.py
@@ -0,0 +1,37 @@
+#Made By James Ryan Wood 
+
+#Imports 
+import os
+import roku
+import time 
+
+
+#Menu 
+os.system("clear")
+os.system("figlet -f slant RoJaker")
+print('------------------------')
+print('Made By James Ryan Wood')
+print('------------------------')
+Roku.discover(timeout=10)
+ip = input("Enter IP: ")
+print("1. Get Info\n2. See Current Running App\n3. Dump Apps\n4. Remote\5.Run A App\n6.Exit")
+menu = input("Menu> ")
+
+
+if menu == "1":
+    roku = Roku(ip)
+    roku.info
+
+elif menu == "2":
+    roku = Roku(ip)
+    roku.active_app
+elif menu == "3":
+    roku = Roku(ip)
+    app = roku.apps[0]
+    print(app.id, app.name, app.version)
+elif menu == "4":
+    print("Work in progress")
+elif menu == "5":
+    print("Work in Progress")
+elif menu == "6":
+    print ("Work In Progress")
