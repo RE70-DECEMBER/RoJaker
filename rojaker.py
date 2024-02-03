d861a2095deffb93198970d0981c2557ee50adbb
RE70-DECEMBER <158739296+RE70-DECEMBER@users.noreply.github.com>
2024-02-03 08:54:12 -0600

    Update beta rojaker.py

--- rojaker.py
+++ rojaker.py
@@ -8,30 +8,33 @@ import time
 
 #Menu 
 os.system("clear")
+result = input("Enter IP: ")
+os.system("clear")
 os.system("figlet -f slant RoJaker")
 print('------------------------')
 print('Made By James Ryan Wood')
+print("IP Slected "+result)
 print('------------------------')
-roku.discover(timeout=10)
-ip = input("Enter IP: ")
-print("1. Get Info\n2. See Current Running App\n3. Dump Apps\n4. Remote\5.Run A App\n6.Exit")
+roku = Roku(result)
+print("1. Get Info\n2. See Current Running App\n3. Dump Apps\n4. Remote\5.Run A App\n6.home menu")
 menu = input("Menu> ")
 
 
 if menu == "1":
-    roku = Roku(ip)
-    roku.info
-
+    print(roku.info)
+    input("press enter to return to menu")
+   
 elif menu == "2":
-    roku = Roku(ip)
-    a.active_app
+    print(roku.active_app)
 elif menu == "3":
-    Roku = Roku(ip)
-    app = a.apps[0]
+    app = roku.apps[0]
     print(app.id, app.name, app.version)
 elif menu == "4":
     print("Work in progress")
 elif menu == "5":
-    print("Work in Progress")
+   roku.discover()
 elif menu == "6":
-    print ("Work In Progress")
+   roku.home() 
+   print("command launched")
+else: 
+    print("Invaild menu slection!")
